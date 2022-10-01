e495fd795a7e06e695877d8054dbca3607084a2d
Dương Nghĩa Hiệp <0306191025@caothang.edu.vn>
2022-10-01 14:24:15 +0700

    update home

--- app_trac_nghiem/lib/home.dart
+++ app_trac_nghiem/lib/home.dart
@@ -28,7 +28,7 @@ Stack _news = Stack(
       ),
     ),
     Padding(
-      padding: const EdgeInsets.fromLTRB(70, 300, 0, 0),
+      padding: const EdgeInsets.fromLTRB(55, 300, 0, 0),
       child: Container(
         width: 300,
         height: 100,
@@ -48,104 +48,39 @@ Stack _news = Stack(
   ],
 );
 //
-
-// friend cua screen friend
-// Container _friend = Container(
-//   height: 100,
-//   width: 400,
-//   decoration: BoxDecoration(
-//       borderRadius: BorderRadius.circular(20), color: Colors.grey[300]),
-//   child: ListTile(
-//     leading: Image.asset(
-//       'assets/images/monkeylogo.png',
-//       width: 50,
-//       height: 50,
-//     ),
-//     title: Column(
-//       children: [const Text('Top 1'), Text('Tổng Điểm: 9999')],
-//     ),
-//     subtitle: Row(mainAxisAlignment: MainAxisAlignment.spaceAround, children: [
-//       const Text('Thắng 10'),
-//       IconButton(
-//         color: Colors.amber,
-//         onPressed: () {},
-//         icon: const Icon(Icons.abc),
-//       ),
-//     ]),
-//   ),
-// );
-//
-
-ListView _friendnnnn = ListView.builder(
+// friend
+ListView _friend = ListView.builder(
     scrollDirection: Axis.vertical,
-    padding: const EdgeInsets.all(8),
-    //itemCount: entries.length,
-    itemCount: 20,
-    itemBuilder: (BuildContext context, int index) {
-      return Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Container(
-              alignment: Alignment.center,
-              width: 150,
-              height: 150,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(30),
-                color: Colors.amber[100],
-              ),
-              // child: ListTile(
-              //   leading: CircleAvatar(
-              //     radius: 50,
-              //     child: CircleAvatar(
-              //       radius: 300,
-              //       backgroundImage: AssetImage('assets/images/hinhgaidep.jpg'),
-              //       // backgroundImage: AssetImage('assets/images/hinhgaidep.jpg'),
-              //     ),
-              //   ),
-              // ),
-              child: Row(
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.all(10),
-                    child: Container(
-                      height: 80,
-                      width: 80,
-                      decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(20)),
-                      child: CircleAvatar(
-                          radius: 50,
-                          child: CircleAvatar(
-                            radius: 100,
-                            backgroundImage:
-                                AssetImage('assets/images/hinhgaidep.jpg'),
-                          )),
-                    ),
-                  ),
-                  Column(
-                    children: [
-                      Container(
-                        width: 20,
-                        height: 20,
-                        color: Colors.blue,
-                      )
-                    ],
-                  ),
-                  Spacer(),
-                  Text('data')
-                ],
-              )));
-    });
-ListView _test = ListView.builder(
-    scrollDirection: Axis.vertical,
-    padding: const EdgeInsets.all(8),
+    padding: const EdgeInsets.all(10),
     itemCount: 20,
     itemBuilder: (BuildContext context, int index) {
       return Card(
+        color: Color.fromARGB(255, 255, 230, 141),
         child: ListTile(
           leading: CircleAvatar(
+              radius: 40,
               backgroundImage: AssetImage('assets/images/hinhgaidep.jpg')),
-          title: Text('Three-line ListTile'),
-          subtitle: Text('A sufficiently long subtitle warrants three lines.'),
-          trailing: Icon(Icons.more_vert),
+          title: Text('Top 1',
+              textAlign: TextAlign.center,
+              style: TextStyle(fontSize: 18, color: Colors.redAccent)),
+          subtitle: Padding(
+            padding: EdgeInsets.all(10),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Dương Nghĩa Hiệp',
+                  style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black),
+                ),
+                Text('Thắng 10')
+              ],
+            ),
+          ),
+          trailing: IconButton(
+              onPressed: () {}, icon: Icon(Icons.screen_share_outlined)),
           isThreeLine: true,
         ),
       );
@@ -155,6 +90,45 @@ ListView _test = ListView.builder(
 class _HomeState extends State<Home> {
   int _selectedIndex = 0;
   static final List<Widget> _widgetOptions = <Widget>[
+    // home
+    Column(children: [
+      Expanded(
+          flex: 0,
+          child: Column(
+            children: const [
+              Padding(
+                padding: EdgeInsets.fromLTRB(0, 10, 0, 20),
+                child: Text(
+                  'Tin tức mới',
+                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.fromLTRB(0, 0, 200, 0),
+                child: Text('Công Nghệ',
+                    style:
+                        TextStyle(fontSize: 32, fontWeight: FontWeight.bold)),
+              ),
+              Padding(
+                padding: EdgeInsets.fromLTRB(0, 0, 150, 5),
+                child: Text(
+                  'Ngày 20 tháng 9 năm 2022',
+                  style: TextStyle(fontSize: 13),
+                ),
+              ),
+            ],
+          )),
+      SizedBox(
+        height: 500,
+        child: ListView.builder(
+            itemCount: 10,
+            itemBuilder: (BuildContext context, int index) {
+              return Column(
+                children: [_news],
+              );
+            }),
+      ),
+    ]),
     // friend
     Column(
       crossAxisAlignment: CrossAxisAlignment.center,
@@ -175,47 +149,11 @@ class _HomeState extends State<Home> {
         ),
         SingleChildScrollView(
           scrollDirection: Axis.vertical,
-          child: SizedBox(height: 555, child: _test),
+          child: SizedBox(height: 555, child: _friend),
         ),
       ],
     ),
     //
-    // home
-    SingleChildScrollView(
-      scrollDirection: Axis.vertical,
-      child: Column(children: [
-        Expanded(
-            flex: 0,
-            child: Column(
-              children: const [
-                Padding(
-                  padding: EdgeInsets.fromLTRB(0, 10, 0, 20),
-                  child: Text(
-                    'Tin tức mới',
-                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-                  ),
-                ),
-                Padding(
-                  padding: EdgeInsets.fromLTRB(0, 0, 200, 0),
-                  child: Text('Công Nghệ',
-                      style:
-                          TextStyle(fontSize: 32, fontWeight: FontWeight.bold)),
-                ),
-                Padding(
-                  padding: EdgeInsets.fromLTRB(0, 0, 150, 5),
-                  child: Text(
-                    'Ngày 20 tháng 9 năm 2022',
-                    style: TextStyle(fontSize: 13),
-                  ),
-                ),
-              ],
-            )),
-        _news,
-        _news,
-        _news,
-        _news,
-      ]),
-    ),
 
     const Text(
       'Index 1: Business',
