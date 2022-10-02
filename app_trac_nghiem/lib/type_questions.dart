d1dd7f67e464ea769771391cc6f4a8ce28501927
NguyenCongChinh <0306191009@caothang.edu.vn>
2022-10-02 14:12:23 +0700

    history and type

--- app_trac_nghiem/lib/type_questions.dart
+++ app_trac_nghiem/lib/type_questions.dart
@@ -9,23 +9,77 @@ class TypeQuestions extends StatefulWidget {
   State<TypeQuestions> createState() => _TypeQuestionsState();
 }
 
-Text _nameapp = const Text(
-  'Trắc Nghiệm Nhanh',
-  style: TextStyle(fontSize: 26, fontWeight: FontWeight.bold),
-);
-
 class _TypeQuestionsState extends State<TypeQuestions> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Color.fromARGB(255, 188, 255, 240),
+      appBar: AppBar(
+        backgroundColor: Color.fromARGB(255, 188, 255, 240),
+        actions: <Widget>[
+          IconButton(
+            icon: const Icon(Icons.search),
+            tooltip: 'Tìm kiếm',
+            color: Colors.black,
+            iconSize: 40,
+            onPressed: () {},
+          ),
+        ],
+      ),
       body: Center(
         child: Column(
           children: [
-            Image.asset('assets/images/monkeylogo.png'),
-            _nameapp,
-            const Padding(
-              padding: EdgeInsets.only(bottom: 50),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(
+                'Thể loại',
+                style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+              ),
             ),
+            SizedBox(
+              height: 500,
+              child: ListView.builder(
+                  itemCount: 10,
+                  itemBuilder: (BuildContext context, int index) {
+                    return InkWell(
+                      onTap: () {
+                        print('nhan');
+                      },
+                      child: Padding(
+                        padding: const EdgeInsets.fromLTRB(60, 10, 60, 10),
+                        child: SizedBox(
+                          width: 200,
+                          height: 100,
+                          child: Card(
+                            color: Color.fromARGB(255, 102, 235, 195),
+                            shape: RoundedRectangleBorder(
+                              borderRadius:
+                                  const BorderRadius.all(Radius.circular(12)),
+                            ),
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              mainAxisSize: MainAxisSize.min,
+                              children: <Widget>[
+                                ListTile(
+                                  leading: CircleAvatar(
+                                      radius: 40,
+                                      backgroundImage: AssetImage(
+                                          'assets/images/hinhgaidep.jpg')),
+                                  title: Text(
+                                    'thể thao',
+                                    style: TextStyle(
+                                        fontSize: 25,
+                                        fontWeight: FontWeight.bold),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                    );
+                  }),
+            )
           ],
         ),
       ),
